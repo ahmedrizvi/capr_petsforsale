183e5ae1f25fd978af1d860d9315815e75bc37bd
ahmedrizvi <ahmed.riz117@gmail.com>
2023-04-02 14:45:59 -0400

    fixed web version not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:capr_petsforsale/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
